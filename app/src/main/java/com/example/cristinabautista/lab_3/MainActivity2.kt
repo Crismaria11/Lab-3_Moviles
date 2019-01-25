1f4c723e36a2c437a92c99427db59eeda326c781
Cristina B <bau161260@uvg.edu.gt>
2019-01-24 22:51:55 -0600

    El adapter ahora funciona

--- app/src/main/java/com/example/cristinabautista/lab_3/MainActivity2.kt
+++ app/src/main/java/com/example/cristinabautista/lab_3/MainActivity2.kt
@@ -3,13 +3,18 @@ package com.example.cristinabautista.lab_3
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import java.util.ArrayList
 
 class MainActivity2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val contactList = (this.application as MyApplication).contact
 
 
         val showButtonBack = findViewById<Button>(R.id.buttonBack)
@@ -21,5 +26,25 @@ class MainActivity2 : AppCompatActivity() {
         showButtonCreate.setOnClickListener{val intent = Intent(this, MainActivity3::class.java)
             startActivity(intent)
         }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList)
+        contactList.adapter = adapter
+
+        contactList.setOnClickListener = AdapterView.OnItemClickListener{parent, view, position, id ->
+            contact.
+        }
+
+
     }
 }
+
+
+
+private var <E> ArrayList<E>.setOnClickListener: Unit
+    get() {}
+
+private var <E> ArrayList<E>.adapter: ArrayAdapter<Contact>
+    get() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    set() {}
--- app/src/main/java/com/example/cristinabautista/lab_3/MyApplication.kt
+++ app/src/main/java/com/example/cristinabautista/lab_3/MyApplication.kt
@@ -1,11 +1,20 @@
+/*
+Cristina Bautista
+Carne: 161260
+ */
 package com.example.cristinabautista.lab_3
 
-class MyApplication {
-    private var contact: ArrayList<Contact> = ArrayList()
+import android.app.Application
+
+class MyApplication: Application() {
+
+    var contact: ArrayList<Contact> = ArrayList()
 
     fun add(name: String, phoneNumber: Int, email: String) {
         val contact: Contact = Contact(name, phoneNumber, email)
         this.contact.add(contact)
     }
+
+
 
 }
